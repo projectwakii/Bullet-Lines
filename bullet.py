12a84a8a541aab77ed1e807b432261683283aab9
David Liu <projectwakii@gmail.com>
2016-08-29 18:06:04 +1000

    Added bullet.sh
    To now get bullet.py working with Keyboard Maestro, I had to add a
    basic shell script.
    
    In order to make the shell script executable, type “chmod +x bullet.sh”
    into Terminal. The shell script can now be used.

--- bullet.py
+++ bullet.py
@@ -13,11 +13,21 @@ for i in range(0, len(corpus)):
     corpusArray.append(corpus[i])
 
 # Add a newline
+increment = 0 # see below
+
 for i in range(0, len(corpus)): # indexing starts from 0
-    if corpus[i] == "•":
-        corpusArray.insert(i, "\n")
+    if corpus[i] == "•": #note that every time a bullet point is inserted, the index actually changes +1
+    # eg with just this:
+    #    the input is a•ab•abc•abcd•abcde
+    #    corpusArray.insert(i, "\n")
+    # corpusArray becomes this:
+    #    ['a', '\n', '•', 'a', '\n', 'b', '•', 'a', '\n', 'b', 'c', '•', ...]
+    # so we need to make sure to add an increment, to make sure it's inserted at the right place
+        corpusArray.insert((i + increment), "\n")
+        increment += 1
 
 # Unarray it, converting it back to a normal string
+print(corpusArray)
 newCorpus = ''.join(corpusArray)
 print("Final result: " + newCorpus)
 
@@ -25,4 +35,4 @@ print("Final result: " + newCorpus)
 pyperclip.copy(newCorpus)
 
 # The final step, not in this code, is to physically press "Cmd + V" in order
-# to paste it into the document. Again, we'll use Keyboard Maestro. 
+# to paste it into the document. Again, we'll use Keyboard Maestro.
